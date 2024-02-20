7c1e1a2086207fa5b77f862e7663da4a2dc37f09
hedie-rgb <hedie.a91@gmail.com>
2024-02-20 14:39:41 -0500

    login needed to access website

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\98912\Desktop\Django Project\Lushlyrics-insecure\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ lushlyrics-webapp-django-main/main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", views.default, name='default'),
+    path("login/", views.login, name='login'),
+    path("signup/", views.signup, name='signup'),
+    path("playlist/", views.playlist, name='your_playlists'),
+    path("search/", views.search, name='search_page') 
+]
